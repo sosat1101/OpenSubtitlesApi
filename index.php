289834fcbfc6f76099e7c78ffe6593b0b1a42134
sosat1101 <air911.q@gmail.com>
2022-03-07 18:18:41 +0800

    finish composer

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use sosat1101\openSubtitles\OpenSubtitlesImp;
-
-require 'vendor/autoload.php';
-$openSubtitlesImp = new OpenSubtitlesImp();
-$login = $openSubtitlesImp->login("sosat1101", "dfcmbb977");
-var_dump($login);
